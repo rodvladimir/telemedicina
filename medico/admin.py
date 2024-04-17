3c2be29c5dea369aaaa3d36c0977538a068640c6
Vladimir Rodrigues <rodrigues.vladimir@outlook.com>
2024-04-17 20:03:42 -0300

    CadastroMedico

---
+++ medico/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Especialidades, DadosMedico
+
+admin.site.register(Especialidades)
+admin.site.register(DadosMedico)
+
+
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.contrib.messages import get_messages
+from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 def cadastro(request):
@@ -17,15 +18,15 @@ def cadastro(request):
         users = User.objects.filter(username=username)
 
         if users.exists():
-            get_messages.add_message(request, constants.ERROR, "Usuario já existe.")
+            messages.add_message(request, constants.ERROR, "Usuario já existe.")
             return redirect('/usuarios/cadastro')
 
         if senha != confirmar_senha:
-            get_messages.add_message(request, constants.ERROR, "A senha e o confirmar senha devem ser iguais.")
+            messages.add_message(request, constants.ERROR, "A senha e o confirmar senha devem ser iguais.")
             return redirect('/usuarios/cadastro')
 
         if len(senha) < 6:
-            get_messages.add_message(request, constants.ERROR, "A senha deve ter mais de 6 digitos.")
+            messages.add_message(request, constants.ERROR, "A senha deve ter mais de 6 digitos.")
             return redirect('/usuarios/cadastro')
  
         try:
@@ -38,3 +39,22 @@ def cadastro(request):
         except:
             print('Erro 4')
         return redirect('/usuarios/cadastro')
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos.')
+        return redirect('/usuarios/login')
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
