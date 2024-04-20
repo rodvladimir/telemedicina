6f8653af076585952c0ecfd8f3a2b163ea9d281f
Vladimir Rodrigues <rodrigues.vladimir@outlook.com>
2024-04-20 18:05:11 -0300

    ConsultasMedico

--- medico/models.py
+++ medico/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 def is_medico(user):
     return DadosMedico.objects.filter(user=user).exists()
@@ -28,6 +29,12 @@ class DadosMedico(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    @property
+    def proxima_data(self):
+        proxima_data = DatasAbertas.objects.filter(user=self.user).filter(data__gt=datetime.now()).filter(agendado=False).order_by('data').first()
+
+        return proxima_data 
 
 class DatasAbertas(models.Model):
     data = models.DateTimeField()
--- medico/views.py
+++ medico/views.py
@@ -3,7 +3,8 @@ from .models import Especialidades, DadosMedico, is_medico, DatasAbertas
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.messages import constants
-from datetime import datetime
+from datetime import datetime,timedelta
+from paciente.models import Consulta
 
 
 # Create your views here.
@@ -82,3 +83,13 @@ def abrir_horario(request):
         messages.add_message(request, constants.SUCCESS, 'Horario agendado com sucesso!')
 
         return redirect('/medicos/abrir_horario')
+
+def consultas_medico(request):
+    if not is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Somente médicos podem acessar essa página.')
+        return redirect('/usuarios/sair')
+
+    hoje = datetime.now().date()
+    consultas_hoje = Consulta.objects.filter(data_aberta__user=request.user).filter(data_aberta__data__gte=hoje).filter(data_aberta__data__lt=hoje + timedelta(days=1))
+    consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id'))
+    return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes, 'is_medico': is_medico(request.user)})
